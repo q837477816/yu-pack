d326beb97a7b1f68b73e837ad406165f821f6811
yuyu <a837477816@qq.com>
2019-02-20 22:59:55 +0800

    支持简易plugins

--- bin/yu-pack.js
+++ bin/yu-pack.js
@@ -9,5 +9,6 @@ let config = require(path.resolve('webpack.config.js'))
 
 let Compiler = require('../lib/Compiler')
 let compiler = new Compiler(config)
+compiler.hooks.entryOption.call()
 // 标识运行编译
 compiler.run()
